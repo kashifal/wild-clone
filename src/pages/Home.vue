1624016c2632f30c366e80391eb5be84938a1ff2
sanwalsulehri <sanwalsulehri077@gmail.com>
2024-07-30 23:13:01 -0700

    footer done @kashifSulehri

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,11 @@
   <div>
     <h1>Kashif made new changes</h1>
     <h2>H2 added by kashif</h2>
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import Nav from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
 </script>
