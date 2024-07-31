31adfb0b9565decc635ea1ad320f5b2676f510b0
sanwalsulehri <sanwalsulehri077@gmail.com>
2024-07-31 14:01:45 -0700

    done

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -251,15 +251,39 @@
       </div>
     </div>
     <div class="pl-64 z-20">
-      <div class="bg-[url('./assets/herobg.webp')] z-10 relative w-full bg-cover"
+      <div class="bg-[url('./assets/herobg.webp')] z-10 h-fit relative w-full bg-cover"
        >
        <div class="absolute inset-0 w-1/6 -left-10  bg-gradient-to-r from-[#0A1C2D] to-[#0A2334] blur-2xl z-20"></div>
+       <div class="absolute inset-0 w-1/6 -left-60  bg-gradient-to-r from-[#0A1C2D] to-[#0A2334] blur-2xl z-20"></div>
         <div class="max-w-9xl mx-auto z-40 relative py-24 px-[34px]">
           <h2 class="text-white text-3xl font-semibold">Welcome Offer</h2>
           <h1 class="text-5xl text-[#FACC15] font-extrabold pt-1">120% up to $5,000</h1>
           <h2 class="text-white text-2xl pt-1 font-bold">and 75 Free Spins</h2>
-          
+          <button class="text-white py-[11px] my-6 px-7  flex flex-col -space-x-2 text-center items-center justify-center rounded-xl bg-[#3FC02F] hover:bg-[#4FC541]">
+            <h5 class="text-lg font-semibold">Sign Up Now</h5>
+            <em class="text-xs font-medium">Only Takes 30 seconds</em>
+          </button>
+          <div class="flex items-center gap-3 ">
+            <p class="font-bold text-white text-xs">Great</p>
+            <div class="flex space-x-1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 20 20"><path fill="#219653" d="M0 0h24v24H0z"></path><path fill="#fff" d="m10 13.676 3.042-.814L14.312 17zm7-5.349h-5.354L10 3 8.354 8.327H3l4.333 3.302-1.646 5.327 4.334-3.302 2.666-2.025z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 20 20"><path fill="#219653" d="M0 0h24v24H0z"></path><path fill="#fff" d="m10 13.676 3.042-.814L14.312 17zm7-5.349h-5.354L10 3 8.354 8.327H3l4.333 3.302-1.646 5.327 4.334-3.302 2.666-2.025z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 20 20"><path fill="#219653" d="M0 0h24v24H0z"></path><path fill="#fff" d="m10 13.676 3.042-.814L14.312 17zm7-5.349h-5.354L10 3 8.354 8.327H3l4.333 3.302-1.646 5.327 4.334-3.302 2.666-2.025z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 20 20"><path fill="#219653" d="M0 0h24v24H0z"></path><path fill="#fff" d="m10 13.676 3.042-.814L14.312 17zm7-5.349h-5.354L10 3 8.354 8.327H3l4.333 3.302-1.646 5.327 4.334-3.302 2.666-2.025z"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 20 20"><path fill="url(#half-star_svg__a)" d="M0 0h24v24H0z"></path><path fill="#fff" d="m10 13.676 3.042-.814L14.312 17zm7-5.349h-5.354L10 3 8.354 8.327H3l4.333 3.302-1.646 5.327 4.334-3.302 2.666-2.025z"></path><defs><linearGradient id="half-star_svg__a" x1="0" x2="20" y1="10" y2="10" gradientUnits="userSpaceOnUse"><stop stop-color="#219653"></stop><stop offset="0.5" stop-color="#219653"></stop><stop offset="0.5" stop-color="#219653" stop-opacity="0"></stop><stop offset="0.5" stop-color="#D9D9D9"></stop><stop offset="1" stop-color="#D9D9D9"></stop><stop offset="1" stop-color="#219653" stop-opacity="0"></stop><stop offset="1" stop-color="#D9D9D9"></stop></linearGradient></defs> </svg>
+          </div>
+          <p class="font-bold text-white text-xs">180+
+          <span class="font-normal"> reviews on</span> 
+          <div class="flex space-x-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="14" fill="none"><path fill="#219653" d="m7.667 10.48 3.042-.772 1.27 3.917zm7-5.063H9.313L7.667.375 6.021 5.417H.667L5 8.542l-1.646 5.041 4.334-3.125 2.667-1.916z"></path></svg>
+            <span class="leading-0 text-xs font-bold text-white">Trustpilot</span>
         </div>
+          </p>
+          </div>
+        </div>
+      </div>
+      <div class="px-[34px]">
+       <div class="bg-gradient-to-r w-full py-6 from-[#294B41] to-[#1B2C3C]"></div>
       </div>
     <Footer />
   </div>
@@ -321,5 +345,7 @@ import Footer from '../components/Footer.vue';
 .sidebar::-webkit-scrollbar-track {
   background-color: transparent; /* Transparent track background */
 }
+
+
 
 </style>
