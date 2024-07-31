5c1bad25d1d5a82942c8c415cc4b78b4cd447f88
sanwalsulehri <sanwalsulehri077@gmail.com>
2024-07-31 16:46:41 -0700

    card1

---
+++ src/components/Cards1.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="grid grid-cols-2 gap-4 py-10">
+        <div class="sm:bg-[url('./assets/sec3bg1.webp')] p-6 cursor-pointer hover:-mt-4 duration-700 transition-all bg-cover w-full min-h-[320px] rounded-2xl">
+            <div class="text-white flex flex-col justify-end h-full">
+                <h1 class="text-2xl font-bold pb-1">Casino</h1>
+                <p class="text-sm">Explore our exclusive games, live casino, and thrilling slot <br /> games.</p>
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -2,10 +2,12 @@
   <div class="sm:p-20 p-4 bg-[#0A1C2D]">
     <h1>Test All components on this page</h1>
     <Tokens />
+    <CardsOne />
   </div>
 </template>
 
 <script setup>
 import Tokens from '../components/Tokens.vue';
+import CardsOne from '../components/Cards1.vue'
 </script>
 
