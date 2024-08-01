39dfb1139263a3d84d2ce7fe55f8d1dddaf03dca
sanwalsulehri <sanwalsulehri077@gmail.com>
2024-07-31 17:44:52 -0700

    scroll cards

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -3,11 +3,13 @@
     <h1>Test All components on this page</h1>
     <Tokens />
     <CardsOne />
+    <ScrollCards />
   </div>
 </template>
 
 <script setup>
 import Tokens from '../components/Tokens.vue';
-import CardsOne from '../components/Cards1.vue'
+import CardsOne from '../components/Cards1.vue';
+import ScrollCards from '../components/Scroll cards.vue';
 </script>
 
